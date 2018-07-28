b3146a301fde69f572364222739ae8983f6904c3
Alexander Pashkov <alexpashkov123@gmail.com>
2018-07-28 19:57:24 +0300

    add get-balanced-terms

--- src/cmptr/core.clj
+++ src/cmptr/core.clj
@@ -18,12 +18,10 @@
 ;;Polynomial degree: 3
 ;;The polynomial degree is stricly greater than 2, I can't solve.
 
-(defn split-by-equals-sign [str] (str/split str #"="))
+(defn split-by-equals-sign [str])
 
-(defn get-terms [str]
+(defn get-term-strings [str]
   (map first (re-seq #"[\+-]?\s?\d+(\.\d+)?\s\*\sX\^\d+" str)))
-
-
 
 
 (defn parse-term [term]
@@ -32,7 +30,21 @@
                  (str/replace term #"\s" ""))]
     {:coef (Float/parseFloat coef) :deg (Integer/parseInt deg)}))
 
-;;(map parse-term (get-terms "5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0"))
+
+(defn get-parsed-terms [str]
+  (map parse-term (get-term-strings str)))
+
+
+
+(defn get-balanced-terms [str]
+  (let [[left right] (str/split str #"=")]
+    (concat
+      (get-parsed-terms left)
+      (map (fn [a] (assoc a :coef (- (:coef a))))
+        (get-parsed-terms right)))))
+
+
+(get-balanced-terms "5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0")
 
 
 (defn -main
