5343d672f54949450ed62dfee2ebb4327496757a
Alexander Pashkov <alexpashkov123@gmail.com>
2018-08-11 19:16:48 +0300

    add validator

--- src/cmptr/core.clj
+++ src/cmptr/core.clj
@@ -20,144 +20,161 @@
 ;;Polynomial degree: 3
 ;;The polynomial degree is stricly greater than 2, I can't solve.
 
-(defrecord Term [coef deg])
-(defn get-deg [^Term term] (:deg term))
-(defn get-coef [^Term term] (if (nil? term) 0 (float (:coef term))))
-
-
-(defn get-term-strs [eq-str]
-  (-> eq-str
-      (str/replace #"\s" "")
-      (str/split #"[-+=]")))
-  
-
-(defn parse-term [term-str]
-  (let [[match coef _ X _ deg] (re-matches #"^(\d+(\.\d+)?)?(\*?X(\^(\d+))?)?$" term-str)]
-    (when (or (empty? term-str) (nil? match))
-      (throw (ex-info "Term doesn't conform to expected spec." {})))
-    {:coef (bigdec (if (nil? coef) 1 coef))
-     :deg (Integer/parseInt (if X (if deg deg "1") "0"))}))
-
-
-(get-term-strings "8 * X^0 - 6 * X^1 + 0 * X^2 - 5.6 * X^3 = 3 * X^0")
-
-(defn get-term-strings [str]
-  (map first (re-seq #"[\+-]?\s?\d+(\.\d+)?\s\*\sX\^\d+" str)))
-
-(defn parse-term [term]
-  (let [[_ coef _ deg]
-        (re-find #"^([\+-]?\d+(\.\d+)?)\*X\^(\d+)"
-                 (str/replace term #"\s" ""))]
-    (->Term (Float/parseFloat coef) (Integer/parseInt deg))))
-
-(defn get-parsed-terms [str]
-  (map parse-term (get-term-strings str)))
-
-(defn sort-terms
-  ([terms]
-   (sort-terms terms >))
-  ([terms direction]
-   (sort (comparator #(direction (get-deg %1) (get-deg %2))) terms)))
-
-(defn get-moved-left-terms [str]
-  (let [[left right] (str/split str #"=")]
-    (sort-terms
-     (concat (get-parsed-terms left)
-             (map (fn [a] (assoc a :coef (- (:coef a))))
-                  (get-parsed-terms right))))))
-
-(defn validate-same-deg [& terms]
-  (when (> (count (distinct (map get-deg terms))) 1)
-    (throw (ex-info "Terms must be of same degree" {}))))
-
-(defn sum-terms
-  ([] (sum-terms (->Term 0 0)))
-  ([terms]
-   (validate-same-deg terms)
-   (->Term (apply + (map get-coef terms))
-           (get-deg (first terms)))))
-
-(defn reduce-terms [terms]
-  (group-by get-deg
-            (filter
-             #(not (zero? (get-coef %)))
-             (map sum-terms (partition-by get-deg terms)))))
-
-(def test-terms (->> (range 0 3)
-                     (map #(->Term (inc %) %))))
-
-(defn remove-trailing-zeroes [num-str]
-  (str/replace num-str #"\.0$" ""))
-
-;; TODO refactor
-(defn get-formatted-eq-str [terms]
-  (str/replace (str
-                (str/trim
-                 (reduce
-                  #(str
-                    %1
-                    (if (< (get-coef %2) 0) " - " " + ")
-                    (str/replace (str (get-coef %2)) #"(-|\.0$)" "")
-                    " * X^"
-                    (get-deg %2))
-                  ""
-                  (sort-terms (flatten (vals terms)) <)))
-                " = 0")
-               #"^\+\s?"
-               ""))
+;(defrecord Term [coef deg])
+;(defn get-deg [^Term term] (:deg term))
+;(defn get-coef [^Term term] (if (nil? term) 0 (float (:coef term))))
 
 
 
-(defn print-solution [str]
-  (println "The solution is:")
-  (println str))
+
+;(defn eq-str-is-valid? [eq-str]
+;  (let [parts (str/split (remove-spaces eq-str) #"=")]
+;    (if (= (count parts) 2)
+;      parts
+;      false)))
 
 
-(defn solve-linear-eq [reduced-terms]
-  (let [{[variable-term] 1 [constant-term] 0 } reduced-terms]
-    (print-solution
-      (remove-trailing-zeroes
-        (/ (get-coef constant-term)
-           (- (get-coef variable-term)))))))
-
-(defn get-abc [terms]
-  (let [{[{a :coef}] 2 [{b :coef}] 1 [{c :coef}] 0} terms]
-    (map #(if (nil? %) 0 %) [a b c])))
-
-(defn solve-quadratic-eq [reduced-terms]
-  (letfn [(get-discriminant [a b c]
-            (- (* b b) (* 4 (* a c))))
-          (solve [a b c]
-                 (map
-                  #(/ (% (- b) (math/sqrt (get-discriminant a b c))) (* 2 a))
-                  [+ -]))]
-    (let [[a b c] (get-abc reduced-terms)
-          discriminant (get-discriminant a b c)
-          solutions (if (not (neg? discriminant)) (distinct (solve a b c)) nil)]
-      (cond
-        (neg? discriminant)
-        (do
-          (println "Discriminant is strictly negative, there are no solutions."))
-        (zero? discriminant)
-        (do
-          (println "Discriminant is zero, the solution is:")
-          (println (first solutions)))
-        (pos? discriminant)
-        (do
-          (println "Discriminant is strictly positive, the two solutions are:")
-          (doseq [solution solutions]
-            (println solution)))))))
-
-(defn get-terms-max-deg [terms]
-  (if (empty? terms) 0 (first (sort > (keys terms)))))
-
-(defn -main [eq-str]
-  (let [reduced-terms (reduce-terms (get-moved-left-terms eq-str))
-        max-deg (get-terms-max-deg reduced-terms)]
-    (println (str "Reduced form: " (get-formatted-eq-str reduced-terms)))
-    (println (str "Polynomial degree: " max-deg))
-    (cond
-      (= 0 max-deg) (print-solution "All real numbers.")
-      (= 1 max-deg) (solve-linear-eq reduced-terms)
-      (= 2 max-deg) (solve-quadratic-eq reduced-terms)
-      :else (println "The polynomial degree is strictly greater than 2, I can't solve."))))
+;(term-str-is-valid? "-3*X^0")
+;
+;(eq-str-is-valid? "8*X^0 - 6 * X^1 + 0 * X^2 - 5.6 * X^3 = - 3 * X^0")
+;
+;(defn parse-term [term-str]
+;  (let [[match coef _ X _ deg] (re-matches #"^(\d+(\.\d+)?)?(\*?X(\^(\d+))?)?$" term-str)]
+;    (or (term-str-is-valid? term-str)
+;        (throw (ex-info "Term string is not valid." {})))
+;    {:coef (bigdec (if (nil? coef) 1 coef))
+;     :deg  (Integer/parseInt (if X (if deg deg "1") "0"))}))
+;
+;
+;(defn parse-eq [eq-str]
+;  (try
+;    (->> eq-str
+;         (get-term-strs)
+;         (map parse-term))
+;    (catch Exception e nil)))
+;
+;(map first (re-seq term-regex "8*X^0 - 6 * X^1 + 0 * X^2 - 5.6 * X^3 = 3 * X^0"))
+;
+;
+;;(defn get-term-strings [str]
+;;  (map first (re-seq #"[\+-]?\s?\d+(\.\d+)?\s\*\sX\^\d+" str)))
+;;
+;;(defn parse-term [term]
+;;  (let [[_ coef _ deg]
+;;        (re-find #"^([\+-]?\d+(\.\d+)?)\*X\^(\d+)"
+;;                 (str/replace term #"\s" ""))]
+;;    (->Term (Float/parseFloat coef) (Integer/parseInt deg))))
+;
+;
+;
+;(defn get-parsed-terms [str]
+;  (map parse-term (get-term-strings str)))
+;
+;(defn sort-terms
+;  ([terms]
+;   (sort-terms terms >))
+;  ([terms direction]
+;   (sort (comparator #(direction (get-deg %1) (get-deg %2))) terms)))
+;
+;(defn get-moved-left-terms [str]
+;  (let [[left right] (str/split str #"=")]
+;    (sort-terms
+;      (concat (get-parsed-terms left)
+;              (map (fn [a] (assoc a :coef (- (:coef a))))
+;                   (get-parsed-terms right))))))
+;
+;(defn validate-same-deg [& terms]
+;  (when (> (count (distinct (map get-deg terms))) 1)
+;    (throw (ex-info "Terms must be of same degree" {}))))
+;
+;(defn sum-terms
+;  ([] (sum-terms (->Term 0 0)))
+;  ([terms]
+;   (validate-same-deg terms)
+;   (->Term (apply + (map get-coef terms))
+;           (get-deg (first terms)))))
+;
+;(defn reduce-terms [terms]
+;  (group-by get-deg
+;            (filter
+;              #(not (zero? (get-coef %)))
+;              (map sum-terms (partition-by get-deg terms)))))
+;
+;(def test-terms (->> (range 0 3)
+;                     (map #(->Term (inc %) %))))
+;
+;(defn remove-trailing-zeroes [num-str]
+;  (str/replace num-str #"\.0$" ""))
+;
+;;; TODO refactor
+;(defn get-formatted-eq-str [terms]
+;  (str/replace (str
+;                 (str/trim
+;                   (reduce
+;                     #(str
+;                        %1
+;                        (if (< (get-coef %2) 0) " - " " + ")
+;                        (str/replace (str (get-coef %2)) #"(-|\.0$)" "")
+;                        " * X^"
+;                        (get-deg %2))
+;                     ""
+;                     (sort-terms (flatten (vals terms)) <)))
+;                 " = 0")
+;               #"^\+\s?"
+;               ""))
+;
+;
+;
+;(defn print-solution [str]
+;  (println "The solution is:")
+;  (println str))
+;
+;
+;(defn solve-linear-eq [reduced-terms]
+;  (let [{[variable-term] 1 [constant-term] 0} reduced-terms]
+;    (print-solution
+;      (remove-trailing-zeroes
+;        (/ (get-coef constant-term)
+;           (- (get-coef variable-term)))))))
+;
+;(defn get-abc [terms]
+;  (let [{[{a :coef}] 2 [{b :coef}] 1 [{c :coef}] 0} terms]
+;    (map #(if (nil? %) 0 %) [a b c])))
+;
+;(defn solve-quadratic-eq [reduced-terms]
+;  (letfn [(get-discriminant [a b c]
+;            (- (* b b) (* 4 (* a c))))
+;          (solve [a b c]
+;            (map
+;              #(/ (% (- b) (math/sqrt (get-discriminant a b c))) (* 2 a))
+;              [+ -]))]
+;    (let [[a b c] (get-abc reduced-terms)
+;          discriminant (get-discriminant a b c)
+;          solutions (if (not (neg? discriminant)) (distinct (solve a b c)) nil)]
+;      (cond
+;        (neg? discriminant)
+;        (do
+;          (println "Discriminant is strictly negative, there are no solutions."))
+;        (zero? discriminant)
+;        (do
+;          (println "Discriminant is zero, the solution is:")
+;          (println (first solutions)))
+;        (pos? discriminant)
+;        (do
+;          (println "Discriminant is strictly positive, the two solutions are:")
+;          (doseq [solution solutions]
+;            (println solution)))))))
+;
+;(defn get-terms-max-deg [terms]
+;  (if (empty? terms) 0 (first (sort > (keys terms)))))
+;
+;(defn -main [eq-str]
+;  (let [reduced-terms (reduce-terms (get-moved-left-terms eq-str))
+;        max-deg (get-terms-max-deg reduced-terms)]
+;    (println (str "Reduced form: " (get-formatted-eq-str reduced-terms)))
+;    (println (str "Polynomial degree: " max-deg))
+;    (cond
+;      (= 0 max-deg) (print-solution "All real numbers.")
+;      (= 1 max-deg) (solve-linear-eq reduced-terms)
+;      (= 2 max-deg) (solve-quadratic-eq reduced-terms)
+;      :else (println "The polynomial degree is strictly greater than 2, I can't solve."))))
---
+++ src/cmptr/parsing.clj
@@ -0,0 +1,29 @@
+(ns cmptr.parsing
+  (:require [clojure.string :as str]
+            [cmptr.math :as math]))
+
+(def term-regex #"[-+]?(((\d+(\.\d+)?)\*(X(\^\d+)?))|((\d+(\.\d+)?))|(X(\^\d+)?))")
+
+(defn- remove-spaces [str] (str/replace str #"\s" ""))
+
+(defn- term-str-is-valid? [term-str]
+  (let [[match] (re-matches term-regex term-str)]
+    (boolean match)))
+
+(defn- eq-str-is-valid? [eq-str]
+  (let [eq-str-without-spaces (remove-spaces eq-str)]
+    (every?
+      (fn [validator] (apply validator [eq-str-without-spaces]))
+      [#(not (empty? %))
+       #(= (count (str/split % #"=")) 2)
+       #(every? term-str-is-valid? (->> (str/split % #"=")
+                                        (map (fn [part] (str/split part #"(=-|=+|((?!^)[\+\-\=]))")))
+                                        (flatten)))])))
+
+(defn parse-eq-str [eq-str]
+  (let [eq-str (remove-spaces eq-str)]
+    (if (eq-str-is-valid? eq-str))
+    :ok
+    :not-valid))
+
+(parse-eq-str "-5 + 1*X^0+4-9.3*X=-1*X^8")
